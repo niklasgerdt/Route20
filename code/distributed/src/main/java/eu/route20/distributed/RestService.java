c46e22d2cfe7468096e8c22a4865468a4e870c3b
niklasgerdt <gerdtni@gmail.com>
2014-01-26 14:58:04 +0200

    rest

--- code/distributed/src/main/java/eu/route20/distributed/RestService.java
+++ code/distributed/src/main/java/eu/route20/distributed/RestService.java
@@ -3,6 +3,6 @@ package eu.route20.distributed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
-@ApplicationPath("/rest")
+@ApplicationPath("/r20")
 public class RestService extends Application {
 }
