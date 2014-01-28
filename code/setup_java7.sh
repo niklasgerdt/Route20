48db9c22add9017c094cae8eebd365fb5a4e19ef
niklasgerdt <gerdtni@gmail.com>
2014-01-28 21:04:03 +0200

    updaetd env

--- code/setup_java7.sh
+++ code/setup_java7.sh
@@ -1,5 +1,5 @@
 	PATH="$HOME/dev/jdk1.7.0_51/bin:$PATH"
-	PATH="$HOME/dev/eclipse-4.4M4:$PATH"
+	PATH="$HOME/dev/eclipse-4.4:$PATH"
 	PATH="$HOME/dev/apache-maven-3.1.1/bin:$PATH"
     	export JAVA_HOME="$HOME/dev/jdk1.7.0_51"
 
