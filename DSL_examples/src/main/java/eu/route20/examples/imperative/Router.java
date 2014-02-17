f766b3018fabd55c7edc93bc06bc8168051f57bb
niklasgerdt <gerdtni@gmail.com>
2014-02-17 16:33:38 +0200

    topic added

--- DSL_examples/src/main/java/eu/route20/examples/imperative/Router.java
+++ DSL_examples/src/main/java/eu/route20/examples/imperative/Router.java
@@ -2,15 +2,27 @@ package eu.route20.examples.imperative;
 
 import java.util.*;
 
-public class Router{
+public class Router {
 	private List<EventListener> listeners = new ArrayList<EventListener>();
+	private Map<String, List<EventListener>> topicListenerMap = new HashMap<String, List<EventListener>>();
 
-	public void registerListener(EventListener listener){
+	public void registerListener(EventListener listener) {
 		listeners.add(listener);
 	}
+	
+	public void registerListener(EventListener listener, String topic) {
+		if (topicListenerMap.containsKey(topic)){
+			List<EventListener> topicListeners = topicListenerMap.get(topic);
+			topicListeners.add(listener);
+		}
+		else {
+			List<EventListener> topicListeners = new ArrayList<EventListener>();
+			topicListenerMap.put(topic, topicListeners);
+		}
+	}
 
-	public void push(Event e){
-		for (EventListener listener: listeners) {
+	public void push(Event e) {
+		for (EventListener listener : listeners) {
 			listener.onEvent(e);
 		}
 	}
